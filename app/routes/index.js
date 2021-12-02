8a14f389ef5d5f0bbb0d5d4284149bf6b239023b
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 20:05:23 -0600

    Add routs for Admin and Students

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
+import adminRouter from "./admin.js";
+import studentsRouter from "./students.js";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
   res.send("Hello Form Grade Book Router");
 });
+
+router.use("/admin", adminRouter);
+router.use("/students", studentsRouter);
 
 export default router;
