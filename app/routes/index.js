b5893fc836ee588f4bdb271f0ed5f07797726e9a
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 19:51:18 -0600

    Fix arrow notation keys

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-const router = new Router
+const router = new Router();
 
-router.get("/", (_, res) +. {
-  res.send("Hello Form Grade Book Router")
+router.get("/", (_, res) => {
+  res.send("Hello Form Grade Book Router");
 });
 
 export default router;
