c42c2bb41431c841cca5c2dee838a7a9a9c89e58
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 20:41:16 -0600

    Create Register router

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
+import adminController from "../controllers/admin.js";
 
 const router = new Router();
 
 router.get("/", (_, res) => {
   res.send("Hello World from Grade Book Admin");
+});
+
+router.post("/register", (req, res) => {
+  const { userName, password } = req.body;
+  adminController.create(userName, password);
+  res.send("Post Request");
 });
 
 export default router;
