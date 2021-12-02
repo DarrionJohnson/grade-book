eb675db316a16ac75bc545a8c98540b8c54ff813
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 19:53:50 -0600

    Create api rout

--- app/index.js
+++ app/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import config from "./config.js";
+import router from "./routes/index.js";
 
 const app = express();
 
 app.get("/", (_, res) => {
   res.send("Hello World from Grade Book");
 });
+
+app.use("/api", router);
 
 app.listen(config.port, () => {
   console.log(`Server 🏃🏾‍♂️ at: ${config.baseUrl}:${config.port}`);
