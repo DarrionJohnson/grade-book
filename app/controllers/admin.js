a096b9e9404e64a9b621bb3daee3b8b4e32be8e6
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 20:51:10 -0600

    Fill in Controller Admin  file code

---
+++ app/controllers/admin.js
@@ -0,0 +1,32 @@
+import client from "../client.js";
+import config from "../config.js";
+
+const admin = client.db(config.db.name).collection("admin");
+
+export default {
+  async create(userName, password) {
+    // check for an existing user in the DB
+
+    console.log(
+      "User Name:",
+      userName,
+      " || ",
+      "Password:",
+      password,
+      "from Admin Controller."
+    );
+
+    const existingUser = await admin.findOne({ userName });
+
+    if (existingUser) {
+      throw new Error("User already exists");
+    }
+
+    // TODO: Encrypt the Password
+
+    // TODO: Use insertOne to insert an admin into database
+
+    // TODO:
+  },
+  async SharedWorker(userName, password) {},
+};
