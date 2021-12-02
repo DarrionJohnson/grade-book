e1ef9d3c9f79775f8f260818395dcdcd2b5c898c
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-01 19:30:29 -0600

    Add and commit out Faker generate script

---
+++ app/MyFaker.js
@@ -0,0 +1,17 @@
+// import faker from "faker";
+// import { promises as fs } from "fs";
+
+// fs.writeFile(
+//   "db.json",
+//   JSON.stringify(
+//     Array.from({ length: 20 }, () => ({
+//       fname: faker.name.firstName(),
+//       lname: faker.name.lastName(),
+//       email: faker.internet.email(),
+//       phone: faker.phone.phoneNumberFormat(),
+//     })),
+//     null,
+//     2
+//   ),
+//   "utf8"
+// );
