3d55fbd438efd08646ae3ba2c7f3a0845bbf9125
Manav Misra <manavm@visionwebsoft.com>
2021-12-08 20:16:08 -0600

    Protect the students

--- app/routes/students.js
+++ app/routes/students.js
@@ -7,9 +7,15 @@ router.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
-router.post("/", async (_, res) => {
-  const students = await studentsController.index();
-  res.json(students);
+router.post("/", async (req, res) => {
+  // If the req is properly authorized...
+
+  if (req.isAuth) {
+    const students = await studentsController.index();
+    res.json(students);
+  } else {
+    res.status(401).json({ message: "Access Denied" });
+  }
 });
 
 export default router;
