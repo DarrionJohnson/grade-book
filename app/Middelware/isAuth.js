7ccabf3a547c31e629a9f0108e16acc0e91701c9
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-08 20:20:07 -0600

    Protect the students

--- app/Middelware/isAuth.js
+++ app/Middelware/isAuth.js
@@ -1,11 +1,15 @@
-import  config  from "../config.js";
 import jwt from "jsonwebtoken";
+import config from "../config.js";
 
-export default (req, res, next) => {
+export default (req, _, next) => {
   try {
     req.isAuth = jwt.verify(
-      req.headers.authorization.split(" ")[1]),
-    config.encryption.secret);
+      req.headers.authorization.split(" ")[1],
+      config.encryption.secret
+    );
     next();
-  } catch (error) {}
+  } catch {
+    req.isAuth = false;
+    next();
+  }
 };
