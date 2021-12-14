4b503f28fb3b7167de18a83e9b38155f1aaa8276
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-13 21:16:40 -0600

    Add an empty peramitor for crash pervention

--- app/Models/Users/Admin.js
+++ app/Models/Users/Admin.js
@@ -1,7 +1,7 @@
 import User from "./User.js";
 
 export default class Admin extends User {
-  constructor({ userName, password }) {
+  constructor({ userName, password } = {}) {
     super({ userName, password });
     this.role = "ADMIN";
   }
