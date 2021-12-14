8e42b33cdbc0b8fafa33eb4f03153cc183ac7956
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-14 03:41:58 -0600

    Build admin Login route

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -34,4 +34,14 @@ router.post("/register", async (req, res) => {
   // }
 });
 
+router.post("/login", async (req, res) => {
+  try {
+    const jwt = await adminController.show(req.body);
+
+    res.status(200).send(jwt);
+  } catch (message) {
+    res.status(400).json({ message });
+  }
+});
+
 export default router;
