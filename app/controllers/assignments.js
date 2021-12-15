09a00cbd09bc328ec3033f14dc313e99a22ae9a3
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-14 22:17:49 -0600

    Push all new assignments onto all students

--- app/controllers/assignments.js
+++ app/controllers/assignments.js
@@ -1,10 +1,12 @@
 import client from "../client.js";
 import config from "../config.js";
+import students from "./students.js";
 
 const assignments = client.db(config.db.name).collection("assignments");
 
 export default {
-  create(newAssignment) {
-    return assignments.insertOne(newAssignment);
+  async create(newAssignment) {
+    await assignments.insertOne(newAssignment);
+    await students.updateMany({}, { $push: { grades: newAssignment } });
   },
 };
