5bad4b0cce1bc12482a063aad083fdac62da7594
Darrion Johnson <darrion.johnson@student.swic.edu>
2021-12-15 00:03:55 -0600

    Add an update route enter a score

--- app/controllers/students.js
+++ app/controllers/students.js
@@ -1,3 +1,4 @@
+import { ObjectID as objectId } from "mongodb";
 import client from "../client.js";
 import config from "../config.js";
 
@@ -6,5 +7,17 @@ const studentsClient = client.db(config.db.name).collection("students");
 export default {
   index() {
     return studentsClient.find().toArray();
+  },
+
+  async update(id, grade) {
+    const studentQuery = {
+      _id: objectId(id),
+      "grades._id": objectId(grade.id),
+    };
+    const addGrade = {
+      $set: { "grades.$.pointsEarned": grade.pointsEarned },
+    };
+
+    return studentsClient.updateOne(studentQuery, addGrade);
   },
 };
